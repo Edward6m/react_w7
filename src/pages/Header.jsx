6936d663cd3ee03272913072b2d84489b394ab20
Edward6m <dndnomrt@gmail.com>
2025-03-22 14:38:51 +0800

    EsLint

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import OffCanvas from "../component/OffCanvas";
 
